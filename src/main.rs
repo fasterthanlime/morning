f397325d6cbcf04a3a824dadc1cc5fcf30017d3c
Amos Wenger <amoswenger@gmail.com>
2019-10-25 22:52:14 +0200

    Hello world

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("Morning Language")
+        .version("0.1.0")
+        .author("@fasterthanlime's twitch chat")
+        .about("?")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .get_matches();
+
+    let input = matches.value_of("INPUT").unwrap();
+    println!("Should compile {}", input);
+
+    unimplemented!()
+}
